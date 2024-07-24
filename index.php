ac2fb801adfefe23cf71057b0578c59d92d9be68
Nathan David Hill <nathan.hill@gmx.at>
2024-07-23 20:50:36 -0400

    first commit of the day

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="styles.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLR+Jp6b7U5l5Xl5b7l5XZl5l5XZl5P3R5l5b7P3R5" crossorigin="anonymous"></script>
   <title>Tanken</title>
@@ -16,13 +17,13 @@
         <div class="row gy-5">
           <div class="col-6">
             <div class="p-3">
-              <input type="number" class="form-control" name="gasAmount" step="0.01" placeholder="You Will See The Gas Amount Here.">
+              <input type="number" class="form-control" name="gasAmount" min="0" placeholder="Amount">
             </div>
           </div>
         </div>
         <div class="col-6">
           <div class="p-3">
-            <input type="number" class="form-control" name="gasPrice" step="0.01" placeholder="You Will See The Gas Price Here.">
+            <input type="" class="form-control" name="gasPrice" step="0.01" min="0" placeholder="Current Gas Price">
           </div>
         </div>
         <div class="col-6">
@@ -36,14 +37,19 @@
 
     <?php
 
+    session_start();
+
+    $tempTotalPrice = 0;
     $totalPrice = 0;
 
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-      $gasPrice = filter_input(INPUT_POST, "gasPrice", FILTER_SANITIZE_NUMBER_FLOAT);
-      $gasAmount = filter_input(INPUT_POST, "gasAmount", FILTER_SANITIZE_NUMBER_FLOAT);
-      $totalPrice = $gasAmount * $gasPrice;
+      $gasPrice = filter_input(INPUT_POST, "gasPrice");
+      $gasAmount = filter_input(INPUT_POST, "gasAmount");
+      $tempTotalPrice = $gasAmount * $gasPrice;
+      $totalPrice += $tempTotalPrice;
     }
-    echo $totalPrice;
+
+    echo "You have to pay: " . $totalPrice . " Euros.";
     ?>
 
 
